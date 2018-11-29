b9a5d6f2978273d563aaf40a0ee8f1656699b05b
Elias Johansson <elias_johansson@hotmail.se>
2018-11-29 15:43:27 +0100

    Connected color-select to change image instead of just background color

--- src/js/colorSelector.js
+++ src/js/colorSelector.js
@@ -7,24 +7,60 @@ const exterior = document.querySelector(".s3-s1 .product:last-child");
 
 const imgs = {
   watch: {
-    pink: "#D49B9B",
-    green: "#2E3921",
-    black: "#000000"
+    silver: {
+      webp: "/assets/img/desktop/webp/4/clock_silver.webp",
+      original: "/assets/img/desktop/png/4/clock_silver.jpg"
+    },
+    blue: {
+      webp: "/assets/img/desktop/webp/4/clock_blue.webp",
+      original: "/assets/img/desktop/png/4/clock_blue.jpg"
+    },
+    black: {
+      webp: "/assets/img/desktop/webp/4/clock_black.webp",
+      original: "/assets/img/desktop/png/4/clock_black.jpg"
+    }
   },
   miniature: {
-    pink: "#D49B9B",
-    green: "#2E3921",
-    black: "#000000"
+    silver: {
+      webp: "/assets/img/desktop/webp/4/rc_silver.webp",
+      original: "/assets/img/desktop/png/4/rc_silver.jpg"
+    },
+    blue: {
+      webp: "/assets/img/desktop/webp/4/rc_blue.webp",
+      original: "/assets/img/desktop/png/4/rc_blue.jpg"
+    },
+    black: {
+      webp: "/assets/img/desktop/webp/4/rc_black.webp",
+      original: "/assets/img/desktop/png/4/rc_black.jpg"
+    }
   },
   interior: {
-    pink: "#D49B9B",
-    green: "#2E3921",
-    black: "#000000"
+    silver: {
+      webp: "/assets/img/desktop/webp/3/terrafugia_interior_silver.webp",
+      original: "/assets/img/desktop/png/3/terrafugia_interior_silver.jpg"
+    },
+    blue: {
+      webp: "/assets/img/desktop/webp/3/terrafugia_interior_blue.webp",
+      original: "/assets/img/desktop/png/3/terrafugia_interior_blue.jpg"
+    },
+    black: {
+      webp: "/assets/img/desktop/webp/3/terrafugia_interior_black.webp",
+      original: "/assets/img/desktop/png/3/terrafugia_interior_black.jpg"
+    }
   },
   exterior: {
-    pink: "#D49B9B",
-    green: "#2E3921",
-    black: "#000000"
+    silver: {
+      webp: "/assets/img/desktop/webp/3/terrafugia_studio_silver.webp",
+      original: "/assets/img/desktop/png/3/terrafugia_studio_silver.jpg"
+    },
+    blue: {
+      webp: "/assets/img/desktop/webp/3/terrafugia_studio_blue.webp",
+      original: "/assets/img/desktop/png/3/terrafugia_studio_blue.jpg"
+    },
+    black: {
+      webp: "/assets/img/desktop/webp/3/terrafugia_studio_black.webp",
+      original: "/assets/img/desktop/png/3/terrafugia_studio_black.jpg"
+    }
   }
 };
 
@@ -42,7 +78,13 @@ const init = () => {
   });
 };
 
-const changeProductColor = (product, color) =>
-  (eval(product).style.backgroundColor = imgs[product][color]);
+const changeProductColor = (product, color) => {
+  eval(product)
+    .querySelector(".BG")
+    .children[0].setAttribute("srcset", imgs[product][color].webp);
+  eval(product)
+    .querySelector(".BG")
+    .children[1].setAttribute("srcset", imgs[product][color].original);
+};
 
 export default init;
